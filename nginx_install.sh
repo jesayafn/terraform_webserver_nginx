c0abee18924a6dd96a8af55588af42a46febfd62
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-12 13:46:49 +0700

    fix conf for ec2

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
+#! /bin/bash
 
-sudo apt-get update
-
-sudo apt-get install nginx
-
-sudo systemctl enable nginx
+sudo apt-get update && sudo apt-get install nginx && sudo systemctl enable nginx
