34cf8669fbd7d78a638369884a66efee009e122a
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-12 12:27:24 +0700

    fix ippub

--- main.tf
+++ main.tf
@@ -29,9 +29,10 @@ resource "aws_vpc" "experimental_terraform_vpc" {
 }
 
 resource "aws_subnet" "experimental_terraform_vpc_subnet" {
-  vpc_id                  = aws_vpc.experimental_terraform_vpc.id
-  cidr_block              = "10.10.10.0/24"
-  map_public_ip_on_launch = true
+  vpc_id                                      = aws_vpc.experimental_terraform_vpc.id
+  cidr_block                                  = "10.10.10.0/24"
+  map_public_ip_on_launch                     = true
+  enable_resource_name_dns_a_record_on_launch = true
   tags = {
     Name = "experimental_terraform"
   }
@@ -81,7 +82,7 @@ resource "aws_security_group" "experimental_terraform_ec2_sg" {
     to_port     = 0
     protocol    = "ICMP"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
 
   ingress {
     description = "HTTP"
@@ -114,8 +115,8 @@ resource "aws_network_interface" "experimental_terraform_ec2_netinterface0" {
 }
 
 resource "aws_instance" "experimental_terraform_ec2" {
-  ami                         = "ami-0fb653ca2d3203ac1"
-  instance_type               = "t2.micro"
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 
   network_interface {
     network_interface_id = aws_network_interface.experimental_terraform_ec2_netinterface0.id
