0d2d1cad51c74527565ea7004c7404f1479313d6
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-12 14:09:18 +0700

    test sg 3

--- main.tf
+++ main.tf
@@ -78,9 +78,9 @@ resource "aws_security_group" "experimental_terraform_ec2_sg" {
 
   ingress {
     description = "Ping"
-    from_port   = 0
+    from_port   = 8
     to_port     = 0
-    protocol    = "ICMP"
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -91,14 +91,6 @@ resource "aws_security_group" "experimental_terraform_ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # ingress {
-  #   description = "allow all"
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
 
   egress {
     from_port   = 0
