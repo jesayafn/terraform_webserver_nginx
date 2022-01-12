2d7a76a17b86e1897ce31c13cec7c01dbd82910a
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-12 11:51:57 +0700

    fix security group

--- main.tf
+++ main.tf
@@ -72,13 +72,23 @@ resource "aws_security_group" "experimental_terraform_ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Ping"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "ICMP"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
 
   ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
