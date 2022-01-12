7b59431e7f89ad432bd85e47dc80ef25783b81fd
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-12 11:46:48 +0700

    fix wrong conf

--- main.tf
+++ main.tf
@@ -106,7 +106,6 @@ resource "aws_network_interface" "experimental_terraform_ec2_netinterface0" {
 resource "aws_instance" "experimental_terraform_ec2" {
   ami                         = "ami-0fb653ca2d3203ac1"
   instance_type               = "t2.micro"
-  associate_public_ip_address = true
 
   network_interface {
     network_interface_id = aws_network_interface.experimental_terraform_ec2_netinterface0.id
