605b40608fd9b60334c42d1524406c3154cb5d8d
Jesaya Fernando Napitupulu <52324282+jessie-txt@users.noreply.github.com>
2022-01-13 19:21:25 +0700

    Rename resource

--- main.tf
+++ main.tf
@@ -13,15 +13,15 @@ provider "aws" {
   secret_key = var.aws_secretkey
 }
 
-resource "aws_vpc" "experimental_terraform_vpc" {
+resource "aws_vpc" "experimental_terraform" {
   cidr_block = "10.10.0.0/16"
   tags = {
     Name = "experimental_terraform"
   }
 }
 
-resource "aws_subnet" "experimental_terraform_vpc_subnet" {
-  vpc_id                                      = aws_vpc.experimental_terraform_vpc.id
+resource "aws_subnet" "experimental_terraform_public" {
+  vpc_id                                      = aws_vpc.experimental_terraform.id
   cidr_block                                  = "10.10.10.0/24"
   map_public_ip_on_launch                     = true
   enable_resource_name_dns_a_record_on_launch = true
@@ -30,20 +30,20 @@ resource "aws_subnet" "experimental_terraform_vpc_subnet" {
   }
 }
 
-resource "aws_internet_gateway" "experimental_terraform_vpc_igw" {
-  vpc_id = aws_vpc.experimental_terraform_vpc.id
+resource "aws_internet_gateway" "experimental_terraform" {
+  vpc_id = aws_vpc.experimental_terraform.id
 
   tags = {
     Name = "experimental_terraform"
   }
 }
 
-resource "aws_route_table" "experimental_terraform_subnet_routetable" {
-  vpc_id = aws_vpc.experimental_terraform_vpc.id
+resource "aws_route_table" "experimental_terraform_pubsub" {
+  vpc_id = aws_vpc.experimental_terraform.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.experimental_terraform_vpc_igw.id
+    gateway_id = aws_internet_gateway.experimental_terraform.id
   }
 
   tags = {
@@ -51,14 +51,13 @@ resource "aws_route_table" "experimental_terraform_subnet_routetable" {
   }
 }
 
-resource "aws_route_table_association" "experimental_terraform_subnet_routetable_association" {
-  subnet_id      = aws_subnet.experimental_terraform_vpc_subnet.id
-  route_table_id = aws_route_table.experimental_terraform_subnet_routetable.id
+resource "aws_route_table_association" "experimental_terraform_pubsub_routetable" {
+  subnet_id      = aws_subnet.experimental_terraform_public.id
+  route_table_id = aws_route_table.experimental_terraform_pubsub.id
 }
 
-resource "aws_security_group" "experimental_terraform_ec2_sg" {
-  description = "Learn Terraform"
-  vpc_id      = aws_vpc.experimental_terraform_vpc.id
+resource "aws_security_group" "experimental_terraform_ec2" {
+  vpc_id      = aws_vpc.experimental_terraform.id
 
   ingress {
     description = "SSH"
@@ -97,27 +96,30 @@ resource "aws_security_group" "experimental_terraform_ec2_sg" {
   }
 }
 
-resource "aws_network_interface" "experimental_terraform_ec2_netinterface0" {
-  subnet_id       = aws_subnet.experimental_terraform_vpc_subnet.id
+resource "aws_network_interface" "experimental_terraform_ec2_eth0" {
+  subnet_id       = aws_subnet.experimental_terraform_public.id
   private_ips     = ["10.10.10.10"]
-  security_groups = [aws_security_group.experimental_terraform_ec2_sg.id]
+  security_groups = [aws_security_group.experimental_terraform_ec2.id]
   tags = {
-    Name = "experimental_terraform_ec2_netinterface0"
+    Name = "experimental_terraform_ec2_eth0"
   }
 }
 
-resource "aws_instance" "experimental_terraform_ec2" {
+resource "aws_instance" "experimental_terraform" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.experimental_terraform_ec2_netinterface0.id
+    network_interface_id = aws_network_interface.experimental_terraform_ec2_eth0.id
     device_index         = 0
   }
-
   
   user_data = "${file("nginx_install.sh")}"
   root_block_device {
     delete_on_termination = true
+  }
+
+  tags = {
+      Name = "experimental_terraform"
   }
 }
